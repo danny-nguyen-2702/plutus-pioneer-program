0ab25f8f6d1c4d22f8aef56dd4477803db7c473a
Danny Ng <danny.nguyen.2702@gmail.com>
2022-09-21 10:25:46 +0700

    Add off-chain functionalities for the Gift smart contract

--- code/week02/Gift.hs
+++ code/week02/Gift.hs
@@ -60,3 +60,52 @@ valHash = Scripts.validatorHash validator
 -- get address of that script on the blockchain
 scrAddress :: Ledger.Address
 scrAddress = scriptAddress validator
+
+
+---------------------- off-chain code ----------------------
+
+-- Endpoints are ways for the user to trigger something and enter data. For this Gift smartcontract, we will have 2 endpoints for users. User can send money to the Script Address via the 'give' endpoint and user can take money from the Script Address by the 'grab' endpoint.
+type GiftSchema =
+        Endpoint "give" Integer
+    .\/ Endpoint "grab" ()
+
+
+-- give function taking the integer amount of lovelace and create a transaction that send this amount of lovelace to the Script Address. The providing fund transaction doesn't need to include the Validator code, but needs to provide the Datum
+give :: AsContractError e => Integer -> Contract w s e ()
+give amount = do
+    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkI 0) $ Ada.lovelaceValueOf amount -- construct the transaction that send the amount of lovelace to the Script Address
+
+    ledgerTx <- submitTx tx -- submit the transaction
+
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx -- wait for the confirmation
+
+    logInfo @String $ printf "made a gift of %d lovelace" amount
+
+
+
+-- grab function takes all UTXOs of the Script Address and send it to user. The spending fund transaction doesn't need to include the Datum, but needs to provide the Redeemer and the Validator code
+grab :: forall w s e. AsContractError e => Contract w s e ()
+grab = do
+    utxos <- utxosAt scrAddress -- looks up for all UTXOs sitting at this script address
+    let orefs = fst <$> Map.toList utxos -- get all references of these UTXOs
+        lookups =   Constraints.unspentOutputs utxos <> -- tell the wallet where to find all those UTXOs
+                    Constraints.otherScript validator -- tell the wallet about the actual validator (if you want to consume an UTXO sitting at a script address, the spending transaction needs to provide the actual validator code)
+        tx :: TxConstraints Void Void
+        tx = mconcat [mustSpendScriptOutput oref $ Redeemer $ Builtins.mkI 17 | oref <- orefs] -- construct the transaction that spends all UTXOs sitting at the Script Address
+
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+
+    logInfo @String $ "collected gifts"
+
+
+endpoints :: Contract () GiftSchema Text ()
+endpoints = awaitPromise (give' `select` grab') >> endpoints -- the select function offers users choices between the give' and the grab', and then we loop over it again and again recursively
+    where
+        give' = endpoint @"give" give
+        grab' = endpoint @"grab" $ const grab
+
+
+mkSchemaDefinitions ''GiftSchema
+
+mkKnownCurrencies [] -- makes the ADA currency available in the plutus playground
