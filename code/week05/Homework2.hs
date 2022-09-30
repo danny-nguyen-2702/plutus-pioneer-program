7fb32d364d65bc21e35297ff71798390496e5643
Danny Ng <danny.nguyen.2702@gmail.com>
2022-10-01 00:00:47 +0700

    Add solution for homework2

---
+++ code/week05/Homework2.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Week05.Homework2 where
+
+import           Control.Monad          hiding (fmap)
+import qualified Data.Map               as Map
+import           Data.Text              (Text)
+import           Data.Void              (Void)
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
+import qualified PlutusTx
+import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Ledger                 hiding (mint, singleton)
+import           Ledger.Constraints     as Constraints
+import qualified Ledger.Typed.Scripts   as Scripts
+import           Ledger.Value           as Value
+import           Prelude                (IO, Semigroup (..), Show (..), String)
+import           Text.Printf            (printf)
+import           Wallet.Emulator.Wallet
+
+{-# INLINABLE tn #-}
+tn :: TokenName
+tn = TokenName emptyByteString
+
+{-# INLINABLE mkPolicy #-}
+-- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
+-- and where the TokenName will be the empty ByteString.
+mkPolicy :: TxOutRef -> () -> ScriptContext -> Bool
+mkPolicy oref () ctx = traceIfFalse "the given UTXO is not consumed" isConsumedUTXO &&
+                       traceIfFalse "the minting amount is not 1"    onlyMintOne
+  where 
+    txInfo :: TxInfo
+    txInfo = scriptContextTxInfo ctx
+
+    isConsumedUTXO :: Bool
+    isConsumedUTXO = any (\txInInfo -> txInInfoOutRef txInInfo == oref) $ txInfoInputs txInfo
+
+    onlyMintOne :: Bool
+    onlyMintOne = case flattenValue (txInfoMint txInfo) of
+      [(cs, tn', amount)] -> cs == ownCurrencySymbol ctx && tn' == tn && amount == 1
+      _                   -> False
+
+policy :: TxOutRef -> Scripts.MintingPolicy
+policy oref = mkMintingPolicyScript $
+  $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . mkPolicy ||])
+  `PlutusTx.applyCode`
+  PlutusTx.liftCode oref
+
+curSymbol :: TxOutRef -> CurrencySymbol
+curSymbol = scriptCurrencySymbol . policy
+
+type NFTSchema = Endpoint "mint" Address
+
+mint :: Address -> Contract w NFTSchema Text ()
+mint addr = do
+  utxos <- utxosAt addr
+
+  case Map.keys utxos of
+    []        -> Contract.logError @String "no utxo found"
+    oref : _  -> do
+      let val     = Value.singleton (curSymbol oref) "" 1 
+          lookups = Constraints.mintingPolicy (policy oref) <> Constraints.unspentOutputs utxos
+          tx      = Constraints.mustMintValue val <> Constraints.mustSpendPubKeyOutput oref
+      
+      ledgerTx <- submitTxConstraintsWith @Void lookups tx
+      void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+
+      Contract.logInfo @String $ printf "forged %s" (show val)
+
+endpoints :: Contract () NFTSchema Text ()
+endpoints = mint' >> endpoints
+  where
+    mint' = awaitPromise $ endpoint @"mint" mint
+
+test :: IO ()
+test = runEmulatorTraceIO $ do
+    let w1 = knownWallet 1
+        w2 = knownWallet 2
+    h1 <- activateContractWallet w1 endpoints
+    h2 <- activateContractWallet w2 endpoints
+    callEndpoint @"mint" h1 $ mockWalletAddress w1
+    callEndpoint @"mint" h2 $ mockWalletAddress w2
+    void $ Emulator.waitNSlots 1
